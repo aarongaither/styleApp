696afae6a985613018c77c8d3c1268c22de72e42
leenbean16 <shamoonlina@gmail.com>
2017-08-12 04:37:45 -0700

    hi aaron

--- models/index.js
+++ models/index.js
@@ -1,42 +1,42 @@
 'use strict';
 
-const fs        = require('fs');
-const path      = require('path');
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const basename  = path.basename(module.filename);
+const basename = path.basename(module.filename);
 // const env       = process.env.NODE_ENV || 'development';
 // const config    = require(__dirname + '/../config/config.json')[env];
-const db        = {};
+const db = {};
 let sequelize;
 
 const connInfo = process.env.JAWSDB_URL || {
-  port: 3306,
-  host: "127.0.0.1",
-  user: "root",
-  password: "ucsd0417",
-  database: "stylistApp"
+    port: 3306,
+    host: "127.0.0.1",
+    user: "root",
+    password: "display:none;",
+    database: "stylistApp"
 }
 
 if (process.env.JAWSDB_URL) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  var sequelize = new Sequelize(connInfo.database, connInfo.user, connInfo.password, {
-    host: connInfo.host,
-    dialect: 'mysql'
-  });
+    sequelize = new Sequelize(connInfo.database, connInfo.user, connInfo.password, {
+        host: connInfo.host,
+        dialect: 'mysql'
+    });
 }
 
 fs.readdirSync(__dirname).filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  }).forEach(file => {
+}).forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
-  });
+});
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
